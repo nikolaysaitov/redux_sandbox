c9f60af9c5874b5b8ca602032324ee8517d4a336
Nikolay Saitov <saitovkms@gmail.com>
2022-10-24 22:23:25 +0300

    npm i redux react-redux, addCash/getCash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash);
+  console.log(cash);
+
+
+
+const addCash = (cash) => {
+  dispatch({type: "add_cash", payload: cash})
+}
+
+const getCash = (cash) => {
+  dispatch({type: "get_cash", payload: cash})
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{fontSize: "2rem"}}>
+        {cash}
+      </div>
+      <div style={{display: "flex"}}>
+        <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import {createStore} from "redux";
+import { legacy_createStore as createStore} from 'redux'
+import { Provider } from 'react-redux';
+
+const defaultState = {
+  cash: 60,
+}
+
+
+const reducer = (state = defaultState, action ) => {
+  switch (action.type){
+    case "add_cash":
+      return {...state, cash: state.cash + action.payload};
+
+    case "get_cash":
+      return {...state, cash: state.cash - action.payload}
+
+    default:
+      return state;
+  }
+} 
+
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store ={store}>
+<App />
+  </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
