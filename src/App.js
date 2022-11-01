606837fec378180f875a011b8a13fb7b04863907
Nikolay Saitov <saitovkms@gmail.com>
2022-11-01 14:07:29 +0300

    ref

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import {useDispatch, useSelector} from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const dispatch = useDispatch();
-  const cash = useSelector(state => state.cash);
+  const cash = useSelector((state) => state.cash);
   console.log(cash);
 
+  const addCash = (cash) => {
+    dispatch({ type: "add_cash", payload: cash });
+  };
 
-
-const addCash = (cash) => {
-  dispatch({type: "add_cash", payload: cash})
-}
-
-const getCash = (cash) => {
-  dispatch({type: "get_cash", payload: cash})
-}
-
-
+  const getCash = (cash) => {
+    dispatch({ type: "get_cash", payload: cash });
+  };
 
   return (
     <div className="App">
-      <div style={{fontSize: "2rem"}}>
-        {cash}
-      </div>
-      <div style={{display: "flex"}}>
-        <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
-        <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
-
+      <div style={{ fontSize: "2rem" }}>{cash}</div>
+      <div style={{ display: "flex" }}>
+        <button onClick={() => addCash(Number(prompt()))}>
+          Пополнить счет
+        </button>
+        <button onClick={() => getCash(Number(prompt()))}>
+          Снять со счета
+        </button>
       </div>
     </div>
   );
